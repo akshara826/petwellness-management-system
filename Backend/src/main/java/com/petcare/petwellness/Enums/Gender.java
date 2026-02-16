b3a677f51f46dd72c5798a5950a119da9f6ef5b4
Sahil Vishwakarma <vishwakarmasahil643@gmail.com>
2026-02-16 17:44:58 +0530

    Removed unwanted folder

--- Backend/src/main/java/com/petcare/petwellness/Enums/Gender.java
+++
@@ -1,9 +0,0 @@
-package com.petcare.petwellness.Enums;
-
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER,
-    PREFER_NOT_TO_DISCLOSE
-}
--- Backend/src/main/java/com/petcare/petwellness/Service/EmailService.java
+++
@@ -1,5 +0,0 @@
-package com.petcare.petwellness.Service;
-
-public interface EmailService {
-    void sendEmail(String to, String subject, String body);
-}
